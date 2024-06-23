6774fb5928892a75743ff0d11a8996c4b584f969
Elliot-Chin <ychin@unb.ca>
2024-06-23 15:56:47 -0300

    Fixed email addr in resum

--- public/data/Etc.js
+++ public/data/Etc.js
@@ -2,7 +2,7 @@
 
 export const resumeDownloadFileName = 'EC_resume.pdf'
 export const githubName = 'Elliot-Chin'
-export const email = 'elliot.chin.emailjs@unb.ca'
+export const email = 'elliot.chin.emailjs@gmail.com'
 export const portfolioStrLink = 'elliotc.dev'
 export const university = 'University of New Brunswick'
 export const degree = 'Bachelor of Science in Software Enginerring (BScSwE)'
