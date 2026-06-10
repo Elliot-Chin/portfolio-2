4803d2644bd002ecf5cf53b31276e34c79ae8a21
Elliot-Chin <ychin@unb.ca>
2026-06-10 15:10:20 -0400

    added vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "@/utils/ThemeProvider";
 import { NextUIProvider } from "@nextui-org/system";
 import { HomeTopNav } from "@/components/nav/HomeTopNav";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
     return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
             <NextUIProvider>
                 <HomeTopNav />
                 <Component {...pageProps} />
+                <Analytics />
             </NextUIProvider>
         </ThemeProvider>
     )
